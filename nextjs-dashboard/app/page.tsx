9f8591f9919b9b1b2812d6bbb5f2d3ca5b58a97d
senayadanir <senay.adanir@gmail.com>
2026-06-02 15:54:11 +0300

    fix: all typescript and configuration errors

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -23,7 +23,7 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
-          <p className={`text-xl text-gray-600`}>This is the test message.</p>
+          {/*<p className={`text-xl text-gray-600`}>This is the test message.</p>*/}
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
