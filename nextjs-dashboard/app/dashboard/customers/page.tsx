6d0a5ed84d7954f1c361413f542de01b28fa76ee
senayadanir <senay.adanir@gmail.com>
2026-06-08 21:21:44 +0300

    add dynamic and revalidate exports to customers page

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import { Metadata } from "next";
 import { fetchFilteredCustomers } from "@/app/lib/data";
 import CustomersTable from "@/app/ui/customers/table";
