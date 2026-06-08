1f6e061add72dfd7bb400620d7df94b530ffcc4b
senayadanir <senay.adanir@gmail.com>
2026-06-08 22:08:49 +0300

    enhance customers page layout with header and loading state

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -5,6 +5,7 @@ import { Metadata } from "next";
 import { fetchFilteredCustomers } from "@/app/lib/data";
 import CustomersTable from "@/app/ui/customers/table";
 import { lusitana } from "@/app/ui/fonts";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Customers",
@@ -16,7 +17,12 @@ export default async function Page() {
 
   return (
     <main className="w-full">
-      <CustomersTable customers={customers} />
+      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+        Customers
+      </h1>
+      <Suspense fallback={<p>Loading customers...</p>}>
+        <CustomersTable customers={customers} />
+      </Suspense>
     </main>
   );
 }
